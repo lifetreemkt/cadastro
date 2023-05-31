f92a13c5d1adbe503272951b75d978531d0d7c33
lifetreemkt <carlos.rodrigues@lifetreemarketing.com>
2023-05-31 20:52:07 -0300

    app finalizado

--- projeto_cadastro/app_cadastro/views.py
+++ projeto_cadastro/app_cadastro/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .models import Produto
 
 def home(request):
     return render(request, 'produtos/home.html')
+def produtos(request):
+    novo_produto = Produto()
+    novo_produto.nome = request.POST.get('nome')
+    novo_produto.descricao = request.POST.get('descricao')
+    novo_produto.preco = request.POST.get('preco')
+    novo_produto.validade = request.POST.get('validade')
+
+    novo_produto.save() #necessario salvar no banco de dados
+
+    produtos = dict(
+        produtos = Produto.objects.all())
+    return render(request, 'produtos/produtos.html', produtos)
--- projeto_cadastro/projeto_cadastro/urls.py
+++ projeto_cadastro/projeto_cadastro/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from app_cadastro import views
 urlpatterns = [
-    path('',views.home, name='home')]
+    path('',views.home, name='home'),
+    path('produtos/', views.produtos, name='lista_produtos')
+]
